95c9a4d1c7ef9260d27dfc03fccc700038c44d5d
Yinbai Ding <dingyinbai@megalinkware.com>
2020-07-10 12:34:00 -0700

    Updates

---
+++ precache-manifest.10b27626e499eb1cb5242f8064c480ec.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "975ac8fd9592f58a4635a8e1c471d20b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "eac75e35267a731a1544",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "7fe550ac8372f5620315",
+    "url": "/static/js/2.71a1cf7e.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.71a1cf7e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "eac75e35267a731a1544",
+    "url": "/static/js/main.e6997503.chunk.js"
+  },
+  {
+    "revision": "29636ac642f897e1c83f",
+    "url": "/static/js/runtime-main.a1948734.js"
+  }
+]);
